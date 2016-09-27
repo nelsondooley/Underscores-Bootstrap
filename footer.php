41ed743bcc374fd5c8d492f8cada6ef29a6c6982
Jake <jake.nelson-dooley@cmdgroup.com>
2016-09-27 12:29:56 -0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Underscores_Bootstrap
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscores-bootstrap' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'underscores-bootstrap' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores-bootstrap' ), 'underscores-bootstrap', '<a href="http://jakenelsondooley.com/" rel="designer">Jake Nelson-Dooley</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
