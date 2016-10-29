e7993e939010bd788d0a7341ac67c3b26def67f2
Jake Nelson-Dooley <jake.nelson-dooley@cmdgroup.com>
2016-10-29 10:46:58 -0500

    adding bootstrap containers to template pages

--- footer.php
+++ footer.php
@@ -11,15 +11,21 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscores-bootstrap' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'underscores-bootstrap' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores-bootstrap' ), 'underscores-bootstrap', '<a href="http://jakenelsondooley.com/" rel="designer">Jake Nelson-Dooley</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscores-bootstrap' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'underscores-bootstrap' ), 'WordPress' ); ?></a>
+					<span class="sep"> | </span>
+					<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores-bootstrap' ), 'underscores-bootstrap', '<a href="http://jakenelsondooley.com/" rel="designer">Jake Nelson-Dooley</a>' ); ?>
+				</div><!-- .site-info -->
+			</div>
+		</div>
+	</div>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
